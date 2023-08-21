53239060b6e4b1ddf0bacc51db33bc5f46a45593
Arthur Azevedo <arthuraz.1990@gmail.com>
2023-08-21 17:35:11 -0300

    Início da implantação da troca de senha

---
+++ src/main/java/org/br/sistufbackend/model/dto/TrocarSenhaRequestDTO.java
@@ -0,0 +1,12 @@
+package org.br.sistufbackend.model.dto;
+
+import lombok.Data;
+
+@Data
+public class TrocarSenhaRequestDTO {
+
+    private String senhaAtual;
+    private String senhaNova;
+    private String confirmacao;
+
+}
---
+++ src/main/java/org/br/sistufbackend/service/TrocarSenhaService.java
@@ -0,0 +1,7 @@
+package org.br.sistufbackend.service;
+
+import org.br.sistufbackend.model.dto.TrocarSenhaRequestDTO;
+
+public interface TrocarSenhaService {
+    void trocarSenha(TrocarSenhaRequestDTO requestDTO);
+}
